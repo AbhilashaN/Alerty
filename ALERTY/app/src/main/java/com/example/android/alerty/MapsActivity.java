571d64d16bae27e6963b54f3f2922e0b44e294b7
AbhilashaN <abhilashanagpal.in@gmail.com>
2018-07-16 02:27:30 +0530

    final submission

--- ALERTY/app/src/main/java/com/example/android/alerty/MapsActivity.java
+++ ALERTY/app/src/main/java/com/example/android/alerty/MapsActivity.java
@@ -15,6 +15,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.location.Location;
+import android.os.Build;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -27,6 +30,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
@@ -42,8 +47,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
@@ -66,21 +73,23 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Random;
 
 import static com.example.android.alerty.SignInActivity.ID;
 
 @TargetApi(23)
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationChangeListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback, com.google.android.gms.location.LocationListener,PermissionUtils.PermissionResultCallback  {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationChangeListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback, com.google.android.gms.location.LocationListener, PermissionUtils.PermissionResultCallback {
     private static final String TAG = MapsActivity.class.getSimpleName();
     private final static int PLAY_SERVICES_REQUEST = 1000;
     private final static int REQUEST_CHECK_SETTINGS = 2000;
     private int MY_DATA_CHECK_CODE = 0;
     public static TextToSpeech myTTS;
-    private static int DISPLACEMENT = 20;
+    private static int DISPLACEMENT = 5;
     public static Location mLastLocation;
     private LocationRequest mLocationRequest;
     private GoogleApiClient mGoogleApiClient;
@@ -97,17 +106,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     DatabaseReference ref;
     GeoFire geoFire;
     GeoQuery geoQuery;
-    MarkerOptions marker;
-    private Integer num=0;
+    MarkerOptions options;
+    Marker car;
+    Marker marker;
+    private Integer num = 0;
+    HashMap<String, Marker> hm = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        requestLocationUpdates();
 
         mUsername = ANONYMOUS;
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -118,10 +134,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
 
-        ref = FirebaseDatabase.getInstance().getReference();
+        ref = FirebaseDatabase.getInstance().getReference("geofire");
         geoFire = new GeoFire(ref);
-        myTTS.speak("Be Alert! Be Safe!", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
         setUpLocation();
+        final Handler hand = new Handler();
+        hand.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                myTTS.speak("Be Alert! Be Safe!", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
+
+            }
+        }, 3000);
         //permissionUtils = new PermissionUtils(MapsActivity.this);
 
         //permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
@@ -143,6 +167,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -158,21 +183,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return super.onOptionsItemSelected(item);
         }
     }
+
     //needed
     private void setUpLocation() {
-        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)){
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             //Requesting run-time permission
-            ActivityCompat.requestPermissions(this,new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CHECK_SETTINGS);
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CHECK_SETTINGS);
 
-        }
-        else{
-            if (checkPlayServices()){
+        } else {
+            if (checkPlayServices()) {
                 buildGoogleApiClient();
 
             }
         }
     }
+
     protected synchronized void buildGoogleApiClient() {
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -182,8 +208,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .build();
         mGoogleApiClient.connect();
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setInterval(2000);
+        mLocationRequest.setFastestInterval(500);
         mLocationRequest.setSmallestDisplacement(DISPLACEMENT); //min displacement (in metres) between location updates
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
@@ -215,8 +241,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    protected void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+
+        }
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
 
     private void displayLocation() {
+        final Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.car1);
+        final Bitmap bm1 = BitmapFactory.decodeResource(getResources(), R.drawable.mycar);
+
+
+        if (num==0){
+            showToast("There are no vehicles within 500m radius");
+
+            //myTTS.speak("There are no vehicles within 500m radius", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
+        }
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
 
@@ -226,21 +271,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
         if (mLastLocation != null) {
-            latitude = mLastLocation.getLatitude();
-            longitude = mLastLocation.getLongitude();
-            LatLng you = new LatLng(latitude, longitude);
-            final Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.car1);
-            marker = new MarkerOptions().position(you).title("Me");
-            mMap.addMarker(marker);
+            lat = mLastLocation.getLatitude();
+            lon = mLastLocation.getLongitude();
+            LatLng you = new LatLng(lat, lon);
+//            if(marker!=null){
+//                    marker.remove();
+//            }
+            options = new MarkerOptions().position(you).title("Me").draggable(true).icon(BitmapDescriptorFactory.fromBitmap(bm1));
+            marker = mMap.addMarker(options);
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(you, 15.0f)); //15.0f refers to the level of zooming
-            LatLng circle = new LatLng(latitude, longitude);
+            LatLng circle = new LatLng(lat, lon);
             mMap.addCircle(new CircleOptions()
                     .center(circle)
                     .radius(500) //500 m
                     .fillColor(0x220000FF)
                     .strokeColor(Color.BLUE)
                     .strokeWidth(5.0f));
-            geoFire.setLocation(ID, new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+            geoFire.setLocation(ID, new GeoLocation(lat, lon), new GeoFire.CompletionListener() {
                 @Override
                 public void onComplete(String key, DatabaseError error) {
 //                    if (error!=null){
@@ -253,46 +300,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             });
 
-
-            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(circle.latitude, circle.longitude), 0.5f); //0.5 km
+            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(circle.latitude, circle.longitude), 0.5); //0.5 km
             geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
                 @Override
                 public void onKeyEntered(String key, GeoLocation location) {
-                    if (key != ID) {
-                        showToast(String.format("Vehicle entered the search area at [%f,%f]", location.latitude, location.longitude));
-                        num++;
-                        geoFire.setLocation(key, new GeoLocation(location.latitude, location.longitude), new GeoFire.CompletionListener() {
-                            @Override
-                            public void onComplete(String key, DatabaseError error) {
-                                if (error != null) {
-                                    showToast("error");
-                                } else {
+//                    showToast("Entered");
+                {       if (key!=ID){
+                    showToast(String.format("Vehicle entered near you at [%f,%f]", location.latitude, location.longitude));
+                    num++;
+                    LatLng vehicle = new LatLng(location.latitude, location.longitude);
+                    MarkerOptions veh = new MarkerOptions().position(vehicle).title("Vehicle").icon(BitmapDescriptorFactory.fromBitmap(bm));
+                    hm.put(key,car);
+                    car = mMap.addMarker(veh);
+                }
 
-                                    //showToast("SAVED");
-                                }
-                            }
-
-                        });
-                        LatLng vehicle = new LatLng(10.5, 78);
-                        MarkerOptions veh = new MarkerOptions().position(vehicle).title("Vehicle").icon(BitmapDescriptorFactory.fromBitmap(bm));
-
-                        mMap.addMarker(veh);
 
                     }
                     {
-                        showToast("There are " + num + " vehicles within 500m radius");
+                        if (num != 0) {
+                            showToast("There are " + num + " vehicles within 500m radius");
 
-                        myTTS.speak("There are " + num + " vehicles within 500m radius", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
+                            myTTS.speak("There are " + num + " vehicles within 500m radius", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
+                        }
                     }
 
                 }
 
 
+                @TargetApi(Build.VERSION_CODES.N)
                 @Override
                 public void onKeyExited(String key) {
                     showToast(String.format("Vehcile %s is no longer in the search area", key));
                     showToast("There are " + num + " vehicles within 500m radius");
-                    geoFire.removeLocation(key);
+                    geoFire.getLocation(key, new LocationCallback() {
+                        @Override
+                        public void onLocationResult(String key, GeoLocation location) {
+                            if (location != null) {
+                                hm.remove(key,car) ;
+                                geoFire.removeLocation(key);
+                                car.remove();
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            System.err.println("There was an error getting the GeoFire location: " + databaseError);
+                        }
+                    });
                     myTTS.speak("There are " + num + " vehicles within 500m radius", TextToSpeech.QUEUE_ADD, null, "DEFAULT");
 
 
@@ -301,6 +356,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 @Override
                 public void onKeyMoved(String key, GeoLocation location) {
+                    car = hm.get(key);
+                    marker.remove();
+                    hm.remove(key);
+                    LatLng vehicle = new LatLng(location.latitude, location.longitude);
+                    MarkerOptions veh = new MarkerOptions().position(vehicle).title("Vehicle").icon(BitmapDescriptorFactory.fromBitmap(bm));
+                    hm.put(key,car);
+                    car = mMap.addMarker(veh);
+
                 }
 
                 @Override
@@ -331,16 +394,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+        request.setInterval(2000);
+        request.setFastestInterval(200);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+//        if (permission == PackageManager.PERMISSION_GRANTED) {
+//            // Request location updates and when an update is
+//            // received, store the location in Firebase
+//            client.requestLocationUpdates(request, new LocationCallback() {
 //
-//    private void createLocationRequest() {
 //
-//        mLocationRequest = new LocationRequest();
-//        mLocationRequest.setInterval(10000);
-//        mLocationRequest.setFastestInterval(5000);
-//        mLocationRequest.setSmallestDisplacement(DISPLACEMENT); //min displacement (in metres) between location updates
-//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//                @Override
+//                public void onLocationResult(LocationResult locationResult) {
 //
-//    }
+//                }
+//
+//                @Override
+//                public void onLocationResult(String key, GeoLocation location) {
+//                    geoFire.setLocation(key,location);
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//
+//                }
+//            }, null);
+//        }
+    }
 
     private void showToast(String s) {
         Toast.makeText(this, s, Toast.LENGTH_LONG).show();
@@ -378,7 +463,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-       getLocation();
+        getLocation();
+        startLocationUpdates();
+
+
     }
 
     private void getLocation() {
@@ -475,8 +563,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onLocationChanged(Location location) {
+
+        if (marker!=null){
+        marker.remove();
+        animateMarker(mLastLocation,location);}
         mLastLocation=location;
-        displayLocation();
+
     }
 
 
@@ -508,18 +600,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMyLocationChange(Location location) {
         mLastLocation = location;
+        showToast("onMYlocChanged");
         displayLocation();
     }
+    public void animateMarker(final Location startPosition, final Location toPosition) {
+
+        LatLng temp = new LatLng(toPosition.getLatitude(),toPosition.getLongitude());
+        marker = mMap.addMarker(new MarkerOptions()
+                .position(temp)
+                .title("Me")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.mycar)));
 
 
-//    public void speakwords(String words){
-//        if(myTTS != null) {
-//            myTTS.speak(words, TextToSpeech.QUEUE_ADD, null, "DEFAULT");
-//            showToast("speaking");
-//        }
-//    }
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
 
+        final long duration = 1000;
+        final Interpolator interpolator = new LinearInterpolator();
 
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.getLongitude() + (1 - t)
+                        * startPosition.getLongitude();
+                double lat = t * toPosition.getLatitude() + (1 - t)
+                        * startPosition.getLatitude();
+
+                marker.setPosition(new LatLng(lat, lng));
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+//                    if (hideMarker) {
+//                        marker.setVisible(false);
+//                    } else {
+//                        marker.setVisible(true);
+//                    }
+                }
+            }
+        });
+    }
 
 }
 
